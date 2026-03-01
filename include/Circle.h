a814288a8e34eb309b809f82fbc4f72240363c59
stuv140 <stuv.otus@yandex.ru>
2026-03-01 17:22:38 +0300

    Create Circle.h

---
+++ include/Circle.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+
+#include "GraphicPrimitive.h"
+
+class Circle: public GraphicPrimitive
+{
+private:
+    Point center_;
+    int radius_;
+
+public:
+    Circle(Point center, int radius) : center_(center), radius_(radius) {}
+    
+    void draw() const override;
+    Point GetCenter()const { return center_; }
+    int GetRadius()const { return radius_; }
+    virtual ~Circle() {};
+};
