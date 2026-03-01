a3814ebcea3c38a05d392aa8e10502eadd135dd1
stuv140 <stuv.otus@yandex.ru>
2026-03-01 17:19:30 +0300

    Create Line.cpp

---
+++ src/Line.cpp
@@ -0,0 +1,8 @@
+
+#include "Line.h"
+#include<iostream>
+
+void Line::draw()const {
+	std::cout << "Draw Line: Begin point (" << begin.x << "," << begin.y << ") End point (" << end.x << "," << end.y << ")" << std::endl;
+
+}
