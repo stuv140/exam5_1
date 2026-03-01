e5e13a1066f9367514f3690d9e25974607610345
stuv140 <stuv.otus@yandex.ru>
2026-03-01 17:25:16 +0300

    Create Line.h

---
+++ include/Line.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include"GraphicPrimitive.h"
+
+class Line: public GraphicPrimitive
+{
+private:
+	Point begin;
+	Point end;
+public:
+	void draw()const override;
+	Line(Point one, Point two):begin(one),end(two){}
+    Line() : begin({}), end({}) {} 
+	Point Get_Begin() const  { return begin; }
+	Point Get_end()const  { return end; }
+   virtual ~Line() override =default;
+
+};
