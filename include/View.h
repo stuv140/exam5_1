8d2cad1010f951266a182db987fa857ae96ee335
stuv140 <stuv.otus@yandex.ru>
2026-03-01 17:26:31 +0300

    Create View.h

---
+++ include/View.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <string>
+#include"Document.h"
+#include<iostream>
+
+class View
+{
+public:
+    virtual ~View() = default;
+    virtual void showMessage(const std::string& msg) = 0;
+    virtual void displayDocument(const Document& doc) = 0;
+};
+
+class ConsoleView :public View
+{
+public:
+    void showMessage(const std::string& msg) override;
+    void displayDocument(const Document& doc) override;
+    virtual ~ConsoleView() {};
+};
