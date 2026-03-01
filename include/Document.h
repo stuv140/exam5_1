747ce0eea13a9204c530dc75f23868733ac0cbd1
stuv140 <stuv.otus@yandex.ru>
2026-03-01 17:24:03 +0300

    Create Document.h

---
+++ include/Document.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include<string>
+#include<vector>
+#include<memory>
+#include"GraphicPrimitive.h"
+//#include "Line.h"
+//#include "Circle.h"
+//#include "Rectangles.h"
+class Document {
+public:
+    void createNew();
+    bool importFromFile(const std::string& filePath);
+    bool exportToFile(const std::string& filePath);
+    void addPrimitive(std::unique_ptr<GraphicPrimitive>&& primitive);
+    void drawAllPrimitives() const;
+    void clearPrimitives();
+    size_t getPrimitivesCount() const {
+        return primitives.size();
+    }
+    void removePrimitiveAt(size_t index);
+   
+private:
+    std::vector<std::unique_ptr<GraphicPrimitive>> primitives;
+};
