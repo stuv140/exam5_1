1e6c1f5868953e07ef3a71ac81e0eca4472a7a04
stuv140 <stuv.otus@yandex.ru>
2026-03-01 17:23:34 +0300

    Create Controller.h

---
+++ include/Controller.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "View.h"
+#include "Document.h"
+#include "GraphicPrimitive.h"
+
+class Controller
+{
+public:
+	Controller(Document* _doc, View* _view);
+    void CreateNew();
+    void Load(const std::string& filename);    
+    void Save(const std::string& filename);
+    void AddPrimitive(std::unique_ptr<GraphicPrimitive>&& primitive);
+    void RemovePrimitive(size_t index);
+    void displayDocument() {
+        view->displayDocument(*doc);
+    }
+private:
+    void updateView() {
+        view->displayDocument(*doc);
+    }
+    Document* doc;
+    View* view;
+};
